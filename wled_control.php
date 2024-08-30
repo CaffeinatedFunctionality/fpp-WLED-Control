3dce9ed7d539cc0e7eaa8a73d5e83ca3f02de4ce
Taylor Mitchell <tmitchell@optimumhit.com>
2024-08-30 16:03:19 -0400

    Update WLED control php file

--- wled_control.php
+++ wled_control.php
@@ -16,75 +16,127 @@ require_once("common.php");
 </head>
 <body>
     <div class="top">
-        <button class="top-btn" id="powerButton"><i class="material-icons">power_settings_new</i>Power</button>
+        <button id="powerButton">
+            <i class="material-icons">power_settings_new</i>
+            <span>Power</span>
+        </button>
         <div class="brightness-control">
-          <span>Brightness</span>
-          <input type="range" id="brightnessSlider" min="0" max="100" value="50">
+            <i class="material-icons">brightness_medium</i>
+            <input type="range" id="brightnessSlider" min="0" max="100" value="50">
         </div>
+        <button id="settingsButton">
+            <i class="material-icons">settings</i>
+            <span>Settings</span>
+        </button>
     </div>
     <div class="content">
-        <div id="colorPicker"></div>
-        <div class="color-controls">
-          <div id="brightnessSlider"></div>
-          <div id="saturationSlider"></div>
-          <div class="color-presets">
-              <button class="color-preset" style="background-color: red;"></button>
-              <button class="color-preset" style="background-color: orange;"></button>
-              <button class="color-preset" style="background-color: yellow;"></button>
-              <button class="color-preset" style="background-color: white;"></button>
-              <button class="color-preset" style="background-color: black;"></button>
-              <button class="color-preset" style="background-color: purple;"></button>
-              <button class="color-preset" style="background-color: blue;"></button>
-              <button class="color-preset" style="background-color: teal;"></button>
-              <button class="color-preset" style="background-color: green;"></button>
-              <button class="color-preset random-preset">R</button>
-          </div>
+        <div id="colorsTab" class="tab-content active">
+            <div id="colorPicker"></div>
+            <div class="color-controls">
+                <div id="brightnessSlider"></div>
+                <div id="saturationSlider"></div>
+                <div class="color-presets">
+                    <button class="color-preset" style="background-color: red;"></button>
+                    <button class="color-preset" style="background-color: orange;"></button>
+                    <button class="color-preset" style="background-color: yellow;"></button>
+                    <button class="color-preset" style="background-color: white;"></button>
+                    <button class="color-preset" style="background-color: black;"></button>
+                    <button class="color-preset" style="background-color: purple;"></button>
+                    <button class="color-preset" style="background-color: blue;"></button>
+                    <button class="color-preset" style="background-color: teal;"></button>
+                    <button class="color-preset" style="background-color: green;"></button>
+                    <button class="color-preset random-preset">R</button>
+                </div>
+                <div class="custom-colors">
+                    <button class="custom-color" id="customColor1"></button>
+                    <button class="custom-color" id="customColor2"></button>
+                    <button class="custom-color" id="customColor3"></button>
+                    <button id="saveCustomPalette" class="custom-color">
+                        <i class="material-icons">check</i>
+                    </button>
+                </div>
+            </div>
+            <div class="palette-section">
+                <h3>
+                    <i class="material-icons palette-icon">palette</i>
+                    <span>Color palette</span>
+                </h3>
+                <div class="search-container">
+                    <i class="material-icons search-icon">search</i>
+                    <input type="text" placeholder="Search" class="search-input" oninput="searchPalettes(this.value)">
+                </div>
+                <div id="paletteList"></div>
+            </div>
         </div>
-        <div class="palette-section">
-            <h3>
-                <i class="material-icons palette-icon">palette</i>
-                <span>Color palette</span>
-            </h3>
+
+        <div id="effectsTab" class="tab-content">
+            <div class="effect-controls">
+                <div class="slider-container">
+                    <h3>Effect Speed</h3>
+                    <div class="slider-wrapper">
+                        <i class="material-icons">timer</i>
+                        <input type="range" id="effectSpeedSlider" min="0" max="255" value="100">
+                    </div>
+                </div>
+                <div class="slider-container">
+                    <h3>Effect Intensity</h3>
+                    <div class="slider-wrapper">
+                        <i class="material-icons">whatshot</i>
+                        <input type="range" id="effectIntensitySlider" min="0" max="255" value="100">
+                    </div>
+                </div>
+            </div>
+            <h3 class="effect-mode-title">Effect Mode</h3>
             <div class="search-container">
                 <i class="material-icons search-icon">search</i>
-                <input type="text" placeholder="Search" class="search-input">
+                <input type="text" placeholder="Search effects" class="search-input" oninput="searchEffects(this.value)">
             </div>
-            <div id="paletteList">
-                <button class="palette-btn">
-                    <span class="palette-name">Default</span>
-                    <div class="palette-preview" style="background: linear-gradient(to right, #ff0000, #00ff00, #0000ff);"></div>
-                </button>
-                <button class="palette-btn">
-                    <span class="palette-name">* Color 1</span>
-                    <div class="palette-preview" style="background: #ff0000;"></div>
-                </button>
-                <button class="palette-btn">
-                    <span class="palette-name">* Color Gradient</span>
-                    <div class="palette-preview" style="background: linear-gradient(to right, #ff0000, #00ff00);"></div>
-                </button>
-                <button class="palette-btn">
-                    <span class="palette-name">* Colors 1&2</span>
-                    <div class="palette-preview" style="background: linear-gradient(to right, #ff0000, #0000ff);"></div>
-                </button>
+            <div id="effectList"></div>
+        </div>
+
+        <!-- Add placeholders for other tabs -->
+        <div id="modelsTab" class="tab-content">
+            <h3 class="models-title">Models</h3>
+            <div class="search-container">
+                <i class="material-icons search-icon">search</i>
+                <input type="text" placeholder="Search models" class="search-input" oninput="searchModels(this.value)">
             </div>
+            <div id="modelList"></div>
+        </div>
+
+        <div id="scheduleTab" class="tab-content">
+            <!-- Content for Favorites/Schedule tab -->
         </div>
     </div>
+
     <div class="bottom-tabs">
-        <button class="tab-btn active" data-tab="Colors">
+        <button class="tab-btn active" data-tab="colorsTab">
             <i class="material-icons">palette</i>Colors
         </button>
-        <button class="tab-btn" data-tab="Effects">
+        <button class="tab-btn" data-tab="effectsTab">
             <i class="material-icons">auto_awesome</i>Effects
         </button>
-        <button class="tab-btn" data-tab="Segments">
+        <button class="tab-btn" data-tab="modelsTab">
             <i class="material-icons">view_in_ar</i>Models
         </button>
-        <button class="tab-btn" data-tab="Favorites">
+        <button class="tab-btn" data-tab="scheduleTab">
             <i class="material-icons">schedule</i>Schedule
         </button>
     </div>
+
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>
+    <script src="js/palettes.js"></script>
     <script src="js/wled_control.js"></script>
+
+    <!-- Add this modal for the custom palette form -->
+    <div id="customPaletteModal" class="modal">
+        <div class="modal-content">
+            <h2>Save Custom Palette</h2>
+            <input type="text" id="customPaletteName" placeholder="Enter palette name">
+            <button id="confirmSavePalette">Save</button>
+            <button id="cancelSavePalette">Cancel</button>
+        </div>
+    </div>
 </body>
 </html>
